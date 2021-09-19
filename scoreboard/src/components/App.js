981dc5afc292e542ee94633b607d43cc2bbba82c
Toni <toni@archPC.localdomain>
2021-09-19 19:57:11 +0200

    Added stats

--- scoreboard/src/components/App.js
+++ scoreboard/src/components/App.js
@@ -7,21 +7,41 @@ class App extends Component {
     players: [
       {
         name: "Guil",
+        score: 0,
         id: 1,
       },
       {
         name: "Treasure",
+        score: 0,
         id: 2,
       },
       {
         name: "Ashley",
+        score: 0,
         id: 3,
       },
       {
         name: "James",
+        score: 0,
         id: 4,
       },
     ],
+  };
+
+  handleScoreChange = (index, delta) => {
+    this.setState((prevState) => {
+      const updatedPlayers = [...prevState.players];
+
+      const updatedPlayer = { ...updatedPlayers[index] };
+
+      updatedPlayer.score += delta;
+
+      updatedPlayers[index] = updatedPlayer;
+
+      return {
+        players: updatedPlayers,
+      };
+    });
   };
 
   handleRemovePlayer = (id) => {
@@ -35,14 +55,17 @@ class App extends Component {
   render() {
     return (
       <div className="scoreboard">
-        <Header title="Scoreboard" totalPlayers={this.state.players.length} />
+        <Header title="Scoreboard" players={this.state.players} />
 
         {/* Players list */}
-        {this.state.players.map((player) => (
+        {this.state.players.map((player, index) => (
           <Player
             name={player.name}
+            changeScore={this.handleScoreChange}
+            score={player.score}
             id={player.id}
             key={player.id.toString()}
+            index={index}
             removePlayer={this.handleRemovePlayer}
           />
         ))}
---
+++ scoreboard/src/components/Stats.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Stats = (props) => {
+  const totalPoints = props.players.reduce((total, player) => {
+    return total + player.score;
+  }, 0);
+
+  return (
+    <table className="stats">
+      <tbody>
+        <tr>
+          <td>Players:</td>
+          <td>{props.players.length}</td>
+        </tr>
+        <tr>
+          <td>Total Points:</td>
+          <td>{totalPoints}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default Stats;
