b68616cdd5a7deee2228377d69db91e615457fb7
Toni <toni@archPC.localdomain>
2021-09-20 12:55:11 +0200

    Implemented Counter

---
+++ scoreboard/src/components/Stopwatch.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+
+class Stopwatch extends Component {
+  state = {
+    isRunning: false,
+    elapsedTime: 0,
+    previousTime: 0,
+  };
+
+  componentDidMount() {
+    this.intervalID = setInterval(() => {
+      return this.tick();
+    }, 100);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  tick = () => {
+    if (this.state.isRunning) {
+      const now = Date.now();
+      this.setState((prevState) => ({
+        previousTime: now,
+        elapsedTime: prevState.elapsedTime + (now - this.state.previousTime),
+      }));
+    }
+  };
+
+  handleStopwatch = () => {
+    this.setState((prevState) => ({
+      isRunning: !this.state.isRunning,
+    }));
+    if (!this.state.isRunning) {
+      this.setState({ previousTime: Date.now() });
+    }
+  };
+
+  handleReset = () => {
+    this.setState({ elapsedTime: 0 });
+  };
+
+  render() {
+    return (
+      <div className="stopwatch">
+        <h2>Stopwatch</h2>
+        <span className="stopwatch-time">
+          {Math.floor(this.state.elapsedTime / 1000)}
+        </span>
+        <button onClick={this.handleStopwatch}>
+          {this.state.isRunning ? "Stop" : "Start"}
+        </button>
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    );
+  }
+}
+
+export default Stopwatch;
