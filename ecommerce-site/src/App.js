a12b42a2c4c1c6a551790d00214c0186d7f4bc12
tonim19 <toni.morovic@gmail.com>
2021-11-13 17:03:51 +0100

    Added Header

--- ecommerce-site/src/App.js
+++ ecommerce-site/src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/header/header";
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
