de9f91dfbaa998123413c516ccc9662ba0d6d7dc
tonim19 <toni.morovic@gmail.com>
2021-11-28 13:05:59 +0100

    Displaying cart items in cart item dropdown

--- ecommerce-site/src/components/cart/cart.jsx
+++ ecommerce-site/src/components/cart/cart.jsx
@@ -1,13 +1,25 @@
 import "./cart.css";
+import { connect } from "react-redux";
+import CartItem from "../cart-item/cart-item";
 import CustomButton from "../custom-button/custom-button";
 
-const Cart = () => {
+const Cart = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cartItems.map((cartItem) => {
+          return <CartItem key={cartItem.id} item={cartItem} />;
+        })}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 };
 
-export default Cart;
+const mapStateToProps = ({ cart: { cartItems } }) => {
+  return {
+    cartItems,
+  };
+};
+
+export default connect(mapStateToProps)(Cart);
