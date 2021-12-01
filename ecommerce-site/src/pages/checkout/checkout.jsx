e9f744ff31cbeb7d9659bd19034c8479ec6c5ad7
tonim19 <toni.morovic@gmail.com>
2021-12-01 13:15:45 +0100

    Integrated Stripe payments

--- ecommerce-site/src/pages/checkout/checkout.jsx
+++ ecommerce-site/src/pages/checkout/checkout.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button";
 
 import {
   selectCartItems,
@@ -35,6 +36,11 @@ const CheckoutPage = ({ cartItems, total }) => {
       <div className="total">
         <span>TOTAL: ${total}</span>
       </div>
+      <div className="test-warning">
+        *Please use the following test credit card for the payments* <br /> 4242
+        4242 4242 4242 - Exp: 01/23 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };
